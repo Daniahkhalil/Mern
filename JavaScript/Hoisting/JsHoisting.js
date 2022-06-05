15a15f7ee78590851fb81eeb8c51abe2f5f8559c
Daniahkhalil <dhkhalil99@gmail.com>
2022-06-05 14:37:22 +0300

    first Assigment

---
+++ JavaScript/Hoisting/JsHoisting.js
@@ -0,0 +1,68 @@
+// // GIVEN
+// // the result will undefined .
+
+// console.log(example);
+// var example = "I'm the example!";
+// // the code by the interpreter
+// var example;
+// console.log(example); // logs undefined
+// example = "I'm the example!";
+
+// //AFTER HOISTING BY THE INTERPRETER
+// // After the hoissting , the result will I'm the example.
+// var example;
+// console.log(example); // logs undefined
+// example = "I'm the example!";
+
+//------------------------------------------------------------------------------------------
+//it will give an error beacuase there is no hoisting with let.
+//2. console.log(example);
+// let example = "I'm the example!";    
+//------------------------------------------------------------------------------------------
+//3.var hello = (name) => console.log(`${name}`); 
+//------------------------------------------------------------------------------------------
+//4.var needle = (test)=> console.log('${test}');
+//------------------------------------------------------------------------------------------
+//5.var brendan = (print)=>console.log('${brendan}');
+//------------------------------------------------------------------------------------------
+//6. var food = 'chicken';
+// food = (eat)=>console.log('${food}');
+// console.log(food='rice');
+//------------------------------------------------------------------------------------------
+//7. var food = "fish";
+// var mean = (food)=>console.log('${food}');
+// console.log(food);
+// food = "chicken";
+// console.log(food);
+// console.log(mean='rice');
+//------------------------------------------------------------------------------------------
+//8.      var genre ="disco";
+//  console.log(genre);
+// var rewind= (genre)=> console.log('${genre}');
+//-------------------------------------------------------------------------------------------
+// 9.dojo = "san jose";
+// console.log(dojo);
+// var dojo=(learn)=>{console.log('${dojo}');
+// }
+// console.log(dojo);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
