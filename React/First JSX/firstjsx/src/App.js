16e2131c16de50c267bc82573451640028e9509b
Daniahkhalil <dhkhalil99@gmail.com>
2022-06-07 11:47:20 +0300

    First Jsx

---
+++ React/First JSX/firstjsx/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div >
+     <h1>Hello Dojo!</h1>
+     <h2>Things i need to do :</h2>
+
+     <ul>
+       <li>Learn React</li>
+       <li>Climb MT.Everest</li>
+       <li>Run a marathon</li>
+       <li>Feed the dogs</li>
+     </ul>
+    </div>
+    </>
+  );
+}
+
+export default App;
