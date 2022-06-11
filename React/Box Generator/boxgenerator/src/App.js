d202066a77bc1394250bb51f570687d700f5f980
Daniahkhalil <dhkhalil99@gmail.com>
2022-06-11 16:59:44 +0300

    Box Generate updated 3

--- React/Box Generator/boxgenerator/src/App.js
+++ React/Box Generator/boxgenerator/src/App.js
@@ -5,13 +5,13 @@ import react, { useState } from 'react';
 function App() {
   const [boxes, setBoxes] = useState([]);
 
-  const newBox=(newBox)=> {
+  const box=(newBox)=> {
     setBoxes([...boxes, newBox])
   }
  
   return (
     <div >
-    <Box newBox={newBox}/>
+    <Box newBox={box}/>
     <div style={{display:"flex"}}>
       {
         boxes.map((box, i) => { return <div key={i} style={{ backgroundColor: box.color, width: box.size, height: box.size, marginRight: 5}}>
