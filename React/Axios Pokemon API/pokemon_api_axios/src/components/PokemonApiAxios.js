50ed6e3c6a995cabcfba26defeda18f932aad47e
Daniahkhalil <dhkhalil99@gmail.com>
2022-06-15 14:33:51 +0300

    using Mongodb

---
+++ React/Axios Pokemon API/pokemon_api_axios/src/components/PokemonApiAxios.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const PokemonApiAxios = () => {
+  return (
+    <div>PokemonApiAxios</div>
+  )
+}
